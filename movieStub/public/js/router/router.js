7b1083f3764b68aa55dc6c396ec864d729f22db6
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2017-03-31 11:18:31 +0530

    Ng route fixed

---
+++ movieStub/public/js/router/router.js
@@ -0,0 +1,14 @@
+movieStubApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+                templateUrl: 'tmpl/home.html',
+                controller: 'movieStubController'
+            })
+        .when('/movie/:id', {
+                templateUrl: 'tmpl/movie.html',
+                controller: 'movieDetailsController'
+            })
+        .otherwise({
+                redirectTo: '/'
+            });
+});
